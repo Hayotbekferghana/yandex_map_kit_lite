af352252d0253c328552a680a5cc80524731e28f
Hayotbek <ferghanksiy@gmail.com>
2025-10-27 18:42:51 +0500

    Supported 16KB page size

--- example/android/app/src/main/java/com/unact/yandexmapkitexample/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.unact.yandexmapkitexample;
-
-import androidx.annotation.NonNull;
-
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-
-public class MainActivity extends FlutterActivity {
-}
--- example/lib/assets/theme/color_pallete.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:ui';
-
-const primaryColor = Color(0xFFFC3F1D);
-
-const backgroundLightColor = Color(0xFFF5F5F5);
-const backgroundDarkColor = Color(0xFF121212);
